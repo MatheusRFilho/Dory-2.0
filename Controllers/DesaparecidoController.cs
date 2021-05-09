6d81c06b7612be94984c9e0a7858f710372fa3e6
MATHEUS LIMA <matheus.lima46@fatec.sp.gov.br>
2021-05-09 06:14:04 -0300

    Front end Primeira Etapa de Cadastro de Desaparecido

---
+++ Controllers/DesaparecidoController.cs
@@ -0,0 +1,33 @@
+﻿using Dory2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Dory2.Controllers
+{
+    public class DesaparecidoController : Controller
+    {
+        private Contexto db = new Contexto();
+
+        // GET: Desaparecido
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult InitialRegisterDesaparecido()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult InitialRegisterDesaparecido(InitialRegisterDesaparecido cad)
+        {
+            ViewBag.test = "Teste";
+            return View();
+        }  
+    }
+}
---
+++ Models/VMDesaparecido.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Dory2.Models
+{
+    public class InitialRegisterDesaparecido
+    {
+        [Required]
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Sexo { get; set; }
+
+        public string Etinia { get; set; }
+
+        public string CorOlhos { get; set; }
+
+        public string CorCabelo { get; set; }
+
+        public string TipoSanguineo { get; set; }
+
+        [DataType(DataType.DateTime, ErrorMessage = "Data em formato inválido")]
+        public DateTime DataNascimento { get; set; }
+
+        public string Altura { get; set; }
+
+        public string Peso { get; set; }
+
+        public string Descricao { get; set; }
+
+    }
+}
